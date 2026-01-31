20da5230466193c1df7bb4670f95c4cc067efbf7
Ignacio Flores <inifl99@gmail.com>
2026-01-31 10:25:14 -0300

    intial player definition

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -1,10 +1,24 @@
 import { Scene } from 'phaser';
+import entities from '../entities';
+
+/** Área en la que el player puede moverse: mitad izquierda de la pantalla */
+export function getPlayerMovementBounds(scene: Scene, margin: number = 20): { minX: number; maxX: number; minY: number; maxY: number } {
+    const w = scene.scale.width;
+    const h = scene.scale.height;
+    return {
+        minX: margin,
+        maxX: Math.floor(w / 2) - margin,
+        minY: margin,
+        maxY: h - margin
+    };
+}
 
 export class Game extends Scene
 {
     camera: Phaser.Cameras.Scene2D.Camera;
     background: Phaser.GameObjects.Image;
-    msg_text : Phaser.GameObjects.Text;
+    player: InstanceType<typeof entities.player>;
+    private movementBounds: { minX: number; maxX: number; minY: number; maxY: number };
 
     constructor ()
     {
@@ -19,17 +33,18 @@ export class Game extends Scene
         this.background = this.add.image(512, 384, 'background');
         this.background.setAlpha(0.5);
 
-        this.msg_text = this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        });
-        this.msg_text.setOrigin(0.5);
+        this.movementBounds = getPlayerMovementBounds(this);
+        const { minX, maxX, minY, maxY } = this.movementBounds;
+        const startX = Math.round((minX + maxX) / 2);
+        const startY = maxY - 50;
 
-        this.input.once('pointerdown', () => {
+        this.player = new entities.player(this, startX, startY);
+        this.add.existing(this.player);
 
-            this.scene.start('GameOver');
-
+        this.input.on('pointerdown', (pointer: Phaser.Input.Pointer) => {
+            const x = Phaser.Math.Clamp(pointer.worldX, minX, maxX);
+            const y = Phaser.Math.Clamp(pointer.worldY, minY, maxY);
+            this.player.moveTo(x, y);
         });
     }
 }
